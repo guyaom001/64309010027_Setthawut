3b162d60fce0a16572ccbc89f700f81448a01e67
setthawut <guyaom001@gmail.com>
2022-11-11 13:31:38 +0700

    Create Exam1_64309010027.js

---
+++ week4/Exam1_64309010027.js
@@ -0,0 +1,9 @@
+const kelvin = 293;
+
+const celsius = kelvin -273;
+console.log(celsius);
+
+let Fahrenheit = celsius * (9/5) + 32;
+
+let o = Math.floor (Fahrenheit);
+console.log(`The temperature is ${o} Fahrenheit.`);
