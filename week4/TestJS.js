2c0ebe5a11c603ef0e08d70540786ffd7c7822e5
setthawut <guyaom001@gmail.com>
2022-11-11 13:34:50 +0700

    Create TestJS.js

---
+++ week4/TestJS.js
@@ -0,0 +1,13 @@
+var myName = 'Setthawut';
+console.log(myName);
+
+let lastName = 'Chusamran';
+const age = 21;
+//age = 22;
+var year = 2022;
+let grade = 3.98;
+
+grade = Math.floor(grade);
+console.log(grade);
+
+console.log(`my name is ${myName} ${lastName}`);
