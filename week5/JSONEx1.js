29eda930b5d2325e148e2ee05d284c3bd0004e67
64309010012_Wuttipong <64309010012@tnk.ac.th>
2022-11-18 10:29:38 +0700

    Create JSONEx1.js

---
+++ week5/JSONEx1.js
@@ -0,0 +1,28 @@
+let order = [{
+    "Customer": "Mr.Lee",
+    "ItemOrder": ["A4 paper", "Ruler"],
+    "Qty": [5, 2],
+    "Price": [120, 25]
+
+}, {
+    "Customer": "Mr.Yue",
+    "ItemOrder": ["Flash Drive", "A4 paper"],
+    "Qty": [2, 3],
+    "Price": [130, 120]
+
+}, {
+    "Customer": "Mr.Guy",
+    "ItemOrder": ["Flash Drive", "A4 paper", "M.2"],
+    "Qty": [1, 4, 2],
+    "Price": [199, 20, 499]
+}
+]
+
+//console.log(order[0].Itemorder.length);
+for (i = 0; i < order.length; i++) {
+    let sum =0;
+    for(let j=0;j<order[i].ItemOrder.length;j++){
+        sum += order[i].Qty[j] * order[i].Price[j];
+    }
+    console.log(order[i].Customer,sum);
+}
